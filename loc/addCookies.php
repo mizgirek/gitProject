59c5a793f852d3caa7cee0f3c1ee98428dccedd0
mizgirek <manager3@tescoma-shop.ru>
2016-10-21 11:07:07 +0700

    add cookie

---
+++ loc/addCookies.php
@@ -0,0 +1,8 @@
+<?php
+
+if (SetCookie("Sparta","ThisIsSparta!!",0x6FFFFFFF)) echo "<h3>Вы успешно идентифицированы!</h3>";
+else echo "<h3>Cookie установить не удалось!</h3>";
+
+
+?>
+<img src="img/sparta.jpg" />
